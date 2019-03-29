5edf6099b728005743ba27d19aad6ed6f4cceaf7
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-03-29 20:31:44 +0530

    My first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include "includes/header.php" ?>
+
+<nav>
+    <i class="fas fa-bars"></i>
+    <div class="uls">
+        <ul class="ul ul-left">
+            <li><img src="lib/imgs/logo.png" alt="logo" id="logo"></li>
+            <li><a href="#" class="logo-title" id="logo-title-left">Keuper</a></li>
+            <li><a href="#" class="logo-title" id="logo-title-right">School</a></li>
+        </ul>
+        <ul class="ul ul-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">School</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+    </div>
+</nav>
+
+<section class="section-hero">
+    <p class="hero hero-title">Good start for your child</p>
+    <p class="hero hero-subline">Aenean nonummyendrerit maurhasellus portasce susci varius mi. Cum sociis<br>natoque penatibus et mag dis parturient ontes nascetur ridiculus mus. Nulla du<br>usce feugiat malesuada odio. Morbi nunc odio gravi.</p>
+</section>
+
+<section class="section-hots">
+    <div class="hots gallery">
+        <p id="gallery-title" class="hots-title">Gallery</p>
+        <img id="hots-gallery-img" src="lib/imgs/gallery-img.png" alt="Gallery Img">
+        <hr>
+        <a href="#" class="view-more">View More →</a>
+    </div>
+    <div class="hots events">
+        <p id="events-title" class="hots-title">Latest Events</p>
+        <div class="events event1">
+            <div class="event-date">
+                <div class="event-date-month event-line1"><p>March</p></div>
+                <div class="event-date-day"><p>30</p></div>
+            </div>
+            <div class="event-details">
+                <div class="event-details-title event-line1">Annual Day Celebration</div>
+                <div class="event-details-desc">Turie ntots nascetur riculus mullamale ada odi dio gravida atcur ecuus a lorm ecaistique orci ac sis ultricpharetra gna. Donec accusan malsuad.</div>
+            </div>
+        </div>
+        <div class="events event2">
+            <div class="event-date">
+                <div class="event-date-month event-line1"><p>May</p></div>
+                <div class="event-date-day"><p>05</p></div>
+            </div>
+            <div class="event-details">
+                <div class="event-details-title event-line1">School Reopening</div>
+                <div class="event-details-desc">Turie ntots nascetur riculus mullamale ada odi dio gravida atcur ecuus a lorm ecaistique orci ac sis ultricpharetra gna. Donec accusan malsuad.</div>
+            </div>
+        </div>
+        <hr>
+        <a href="#" class="view-more">View More →</a>
+    </div>
+</section>
+
+<section class="section-offers">
+    hjk
+</section>
+
+<?php include "includes/footer.php" ?>
