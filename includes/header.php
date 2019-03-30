99e62d87e2226e6f97a619a29665c2732da9bd99
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-03-30 14:23:43 +0530

    sticky nav

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Keuper</title>
     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
     <link href="includes/style.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php include "includes/header.php" ?>
 
-<nav>
+<Script>
+    $(document).ready(function() {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 100) {
+                $('#nav').addClass('nav-opacity');
+            }
+            if ($(window).scrollTop() > 400) {
+                $('#nav').removeClass('nav-opacity');
+                $('#nav').addClass('nav-fixed');
+                $('#logo').addClass('logo-small');
+                $('#logo').removeClass('logo');
+            }
+            if ($(window).scrollTop() < 401) {
+                $('#nav').removeClass('nav-fixed');
+                $('#logo').addClass('logo');
+                $('#logo').removeClass('logo-small');
+            }
+        });
+    });
+</Script>
+
+<nav id="nav">
     <i class="fas fa-bars"></i>
     <div class="uls">
         <ul class="ul ul-left">
-            <li><img src="lib/imgs/logo.png" alt="logo" id="logo"></li>
+            <li><img src="lib/imgs/logo.png" alt="logo" id="logo" class="logo"></li>
             <li><a href="#" class="logo-title" id="logo-title-left">Keuper</a></li>
             <li><a href="#" class="logo-title" id="logo-title-right">School</a></li>
         </ul>
