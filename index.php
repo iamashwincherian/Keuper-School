de8722851dabb1b7c235185ebb989429928782e0
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-04-05 08:45:54 +0530

    footer

--- index.php
+++ index.php
@@ -64,8 +64,27 @@
     </div>
 </section>
 
-<section class="section-contact">
+<footer>
+    <div class="footer-content footer-contact">
+        <p class="footer-content-title">Contact</p>
+        <p class="footer-subcontent contact-content">Phone : +91 9567215802</p>
+        <p class="footer-subcontent contact-content">info@keuperschool.com</p>
+        <p class="footer-subcontent contact-content address">#44, Maruthi Meadows, Opp. SABC Bible College, Hennur - Bagalur Main Road, Kothanur Post, Bengaluru, Karnataka 560077</p>
+        
 
-</section>
+    </div>
+    <div class="footer-content footer-center">
+        <p class="footer-content-title footer-content-title-center">KEUPER SCHOOL</p>
+        <p class="footer-content-center-copy footer-subcontent">Copyright &copy 2019, All rights reserved to Keuper</p>
+        <p class="footer-content-center-creator footer-subcontent">Created by Ashwin Cherian Joseph</p>
+    </div>
+    <div class="footer-content footer-follow">
+        <p class="footer-content-title footer-content-title-center-follow">Follow Us</p>
+        <div class="footer-contact-social">
+            <a href="#"><i class="fab fa-facebook-f social-icon social-icon-facebook"></i></a>
+            <a href="#"><i class="fab fa-youtube social-icon social-icon-youtube"></i></a>
+        </div>
+    </div>
+</footer>
 
 <?php include "includes/footer.php" ?>
