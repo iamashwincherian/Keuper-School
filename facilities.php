21f2d086defc20fb585e69cfd0c02b158d2dfbf7
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-04-07 15:41:14 +0530

    gradient

--- facilities.php
+++ facilities.php
@@ -31,7 +31,27 @@
     <div class="foot">
         <img src="./lib/imgs/foot.png" alt="Foot" id="foot-img">
     </div>
+    <footer>
+    <div class="footer-content footer-contact">
+        <p class="footer-content-title">Contact</p>
+        <p class="footer-subcontent contact-content">Phone : +91 9876543210</p>
+        <p class="footer-subcontent contact-content">info@keuperschool.com</p>
+        <p class="footer-subcontent contact-content address">#44, Maruthi Meadows, Opp. SABC Bible College, Hennur - Bagalur Main Road, Kothanur Post, Bengaluru, Karnataka 560077</p>
+        
+
+    </div>
+    <div class="footer-content footer-center">
+        <p class="footer-content-title footer-content-title-center">KEUPER SCHOOL</p>
+        <p class="footer-content-center-copy footer-subcontent">Copyright &copy 2019, All rights reserved to Keuper</p>
+        <p class="footer-content-center-creator footer-subcontent">Created <i class="fas fa-code code-icon"></i> by Ashwin Cherian Joseph</p>
+    </div>
+    <div class="footer-content footer-follow">
+        <p class="footer-content-title footer-content-title-center-follow">Follow Us</p>
+        <div class="footer-contact-social">
+            <a href="#"><i class="fab fa-facebook-f social-icon social-icon-facebook"></i></a>
+            <a href="#"><i class="fab fa-youtube social-icon social-icon-youtube"></i></a>
+        </div>
+    </div>
+</footer>
 </section>
 
-
-<?php include "includes/footer.php" ?>
