6aa3b68c9ffa254ec7f7fa40f99ecb36749f87ae
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-04-08 23:49:30 +0530

    test

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Keuper</title>
-    
+
+    <link rel="shortcut icon" type="image/x-icon" href="./lib/imgs/logo-new.png" />
     <link href="http://allfont.net/allfont.css?fonts=time-roman-bold" rel="stylesheet" type="text/css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
