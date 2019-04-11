3d4dd165e000d449542e0f34fcf8865fc09ae323
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-04-11 16:19:09 +0530

    enquery

--- about.php
+++ about.php
@@ -22,21 +22,92 @@
                     <button type="submit" name="submit" class="inputs send-button">Send</button>
                 </form>
 
-<?php 
+<?php
+    if(isset($_POST['email'])) {
+    
+        // EDIT THE 2 LINES BELOW AS REQUIRED
+        $email_to = "iamashwincherian@gmail.com";
+        $email_subject = "Enquery Email";
+    
+        function died($error) {
+            // your error code can go here
+            echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+            echo "These errors appear below.<br /><br />";
+            echo $error."<br /><br />";
+            echo "Please go back and fix these errors.<br /><br />";
+            die();
+        }
+    
+    
+        // validation expected data exists
+        if(!isset($_POST['name']) ||
+            !isset($_POST['email']) ||
+            !isset($_POST['message'])) {
+            died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        }
+    
+        
+    
+        $name = $_POST['name']; // required
+        $email_from = $_POST['email']; // required
+        $message = $_POST['message']; // required
+    
+        $error_message = "";
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+    
+        if(!preg_match($email_exp,$email_from)) {
+            $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+        }
+        
+            $string_exp = "/^[A-Za-z0-9 .'-]+$/";
+        
+        if(!preg_match($string_exp,$name)) {
+            $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+        }
+        
+        if(strlen($message) < 2) {
+            $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+        }
+        
+        if(strlen($error_message) > 0) {
+            died($error_message);
+        }
+    
+        $email_message = "Enquery Details : \n\n";
+    
+        
+        function clean_string($string) {
+            $bad = array("content-type","bcc:","to:","cc:","href");
+            return str_replace($bad,"",$string);
+        }
 
-    if(isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $subject = "Enquiry";
-        $message = $_POST['message'];
+        $email_message .= "Name: ".clean_string($name)."\n";
+        $email_message .= "Email: ".clean_string($email_from)."\n";
+        $email_message .= "Message: ".clean_string($message)."\n";
+    
+        // create email headers
+        $headers = 'From: '.$email_from."\r\n".
+        'Reply-To: '.$email_from."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+        @mail($email_to, $email_subject, $email_message, $headers);
 
-        $mailTo = "iamashwincherian@gmail.com";
-        $headers = "From: ".$email;
-        $txt = "You have recieved an email from ".$name.".\n\n".$message;
+        echo "<script>
+        alert('Message has been successfully sent!');
+        </script>";
+} 
+?>
 
-        mail($email, $subject, $txt, $headers);
-    }
+<!-- 
+                <form method="post" action="about.php">
+                    <textarea name="message"></textarea>
+                    <input type="submit" name="submit">
+                </form> -->
 
+<?php
+    // if(isset($_POST["message"])) {
+    //     $message = $_POST["message"];
+    //     mail("iamashwincherian@gmail.com", "Form to email message", $_POST["message"], "From: ashwincherianjoseph@gmail.com");
+    // }
 ?>
 
             </div>
