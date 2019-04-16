3cc386dcdb600e116e9a9b36f1956adf5c293120
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2019-04-17 00:19:06 +0530

    admin

---
+++ admin/includes/panels.php
@@ -0,0 +1,32 @@
+<section class="section-panels">
+    <div class="admin-nav">
+        <img src="../lib/imgs/logo-new.png" alt="logo" class="logo-admin">
+        <div class="categories">
+            <div class="category dashboard-button">
+                <i class="fas fa-tachometer-alt panel-icon"></i>
+                <p class="category-title">Dashboard</p>
+            </div>
+            <div class="category dashboard-button">
+                <i class="far fa-images panel-icon"></i>
+                <p class="category-title">Gallery</p>
+            </div>
+            <div class="category dashboard-button">
+                <i class="far fa-calendar-alt panel-icon"></i>
+                <p class="category-title">Events</p>
+            </div>
+            <div class="category dashboard-button">
+                <i class="fas fa-download panel-icon"></i>
+                <p class="category-title">Downlaods</p>
+            </div>
+            <div class="category dashboard-button">
+                <i class="fas fa-sticky-note panel-icon"></i>
+                <p class="category-title">Pop Up</p>
+            </div>
+            <div class="category dashboard-button">
+                <i class="far fa-images panel-icon"></i>
+                <p class="category-title">Settings</p>
+            </div>
+        </div>
+    </div>
+    <div class="admin-body"></div>
+</section>
